b94fa34ece1d8b7bddeea1fc19f922856f8ddfa6
marekl0605 <marekl0605@gmail.com>
2025-06-13 20:36:28 -0400

    add langchain model as a vector

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
+from vector import retriever
 
 model = OllamaLLM(model="llama3.2")
 
@@ -19,7 +20,8 @@ while True:
     print("\n\n")
     if question == "q":
         break
-
-    result = chain.invoke({"reviews": [], "question": question})
+    
+    reviews = retriever.invoke(question)
+    result = chain.invoke({"reviews": reviews, "question": question})
     print(result)
 
